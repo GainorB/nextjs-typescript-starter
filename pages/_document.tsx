a8ee7a290fe1b587376c53154da30b8931bc7a48
Gainor Bostwick <gainorbostwick@gmail.com>
2020-04-03 18:55:02 -0400

    fix: No CSS Flash

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Document, {Head, Main, NextScript} from 'next/document';
+import Document, {Head, Main, NextScript, DocumentContext} from 'next/document';
 import {ServerStyleSheet} from 'styled-components';
 
 interface DocumentProps {
@@ -7,11 +7,29 @@ interface DocumentProps {
 }
 
 export default class MyDocument extends Document<DocumentProps> {
-  static getInitialProps({renderPage}: any) {
+  static async getInitialProps(ctx: DocumentContext) {
     const sheet = new ServerStyleSheet();
-    const page = renderPage((App: any) => (props: any) => sheet.collectStyles(<App {...props} />));
-    const styleTags = sheet.getStyleElement();
-    return {...page, styleTags};
+    const originalRenderPage = ctx.renderPage;
+
+    try {
+      ctx.renderPage = () =>
+        originalRenderPage({
+          enhanceApp: App => props => sheet.collectStyles(<App {...props} />),
+        });
+
+      const initialProps = await Document.getInitialProps(ctx);
+      return {
+        ...initialProps,
+        styles: (
+          <React.Fragment>
+            {initialProps.styles}
+            {sheet.getStyleElement()}
+          </React.Fragment>
+        ),
+      };
+    } finally {
+      sheet.seal();
+    }
   }
 
   render() {
